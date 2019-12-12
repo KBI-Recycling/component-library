106b2dc8535757ff7c84c8cc392e61389406ddc9
Daniel Kinsbursky <dkinsbursky@kinsbursky.com>
2019-12-11 21:47:42 -0800

    Start Building Sample Page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:react/recommended", "react-app", "google"],
+  "rules": {
+    "arrow-parens": 0,
+    "indent": [ "error", 2, {"MemberExpression": 1}],
+    "linebreak-style": 0,
+    "max-len": ["error", {"code": 160}],
+    "react/prop-types": [2],
+    "react/jsx-tag-spacing": [2],
+  }
 };
--- examples/src/index.js
+++ examples/src/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {render} from 'react-dom';
+import {AppBar, CssBaseline, Divider, Drawer, ListItem, ListItemText, Toolbar, Typography} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 import {TestComponent} from '../../src';
 
-const App = () => (
-  <TestComponent />
-);
+const App = () => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <CssBaseline />
+      <AppBar position='fixed' className={classes.appBar}>
+        <Toolbar />
+      </AppBar>
+      <Drawer className={classes.drawer} variant='permanent' classes={{paper: classes.drawerPaper}} anchor='left'>
+        <Toolbar>
+          <Typography variant='h6'>
+            material-formik-ui
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary='Checkbox' />
+        </ListItem>
+      </Drawer>
+      <TestComponent />
+    </Fragment>
+  );
+};
+
+const drawerWidth = 240;
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+}));
 render(<App />, document.getElementById('root'));
