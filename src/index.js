d70500ca7e7062cfd1b25adf155e38be2f7a1047
Daniel Kinsbursky <dkinsbursky@kinsbursky.com>
2020-01-02 22:46:20 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import {default as FormikForm} from './formik/FormikForm';
-import {default as SubmitButton} from './formik/SubmitButton';
+import {default as FormikForm} from './Formik/FormikForm';
+import {default as SubmitButton} from './Formik/SubmitButton';
 
 export {default as Hello} from './TestComponent/Hello';
 export const Formik = {FormikForm, SubmitButton};
