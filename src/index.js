f6c1383482255df12b488084fdf1f4139e977630
Daniel Kinsbursky <dkinsbursky@kinsbursky.com>
2020-01-15 16:28:26 -0800

    Export CurrencyField was missing;  Patched;

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import {default as FormikForm} from './Formik/FormikForm';
 import {default as SubmitButton} from './Formik/SubmitButton';
 import {default as FormButton} from './Formik/FormButton';
-import {default as TextField} from './Formik/TextField';
 import {default as AutoComplete} from './Formik/AutoComplete';
 import {default as AutoCompleteObject} from './Formik/AutoCompleteObject';
-import {default as NumberField} from './Formik/NumberField';
+import {default as CurrencyField} from './Formik/CurrencyField';
 import {default as DateField} from './Formik/DateField';
+import {default as NumberField} from './Formik/NumberField';
 import {default as PasswordField} from './Formik/PasswordField';
 import {default as RadioGroup} from './Formik/RadioGroup';
 import {default as SelectField} from './Formik/SelectField';
 import {default as SwitchField} from './Formik/SwitchField';
+import {default as TextField} from './Formik/TextField';
 import {default as WeightField} from './Formik/WeightField';
 
 export {default as ValidationExample} from './TestComponent/ValidationExample';
@@ -19,18 +20,15 @@ export const Formik = {
   FormikForm,
   SubmitButton,
   FormButton,
-  TextField,
   AutoComplete,
-  NumberField,
+  AutoCompleteObject,
+  CurrencyField,
   DateField,
+  NumberField,
   PasswordField,
   RadioGroup,
   SelectField,
-  AutoCompleteObject,
   SwitchField,
+  TextField,
   WeightField,
 };
-/*
-export {default as Formik} from './Formik/';
-export * from './Formik/';
-*/
