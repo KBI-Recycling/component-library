5906a82acb66454a3700033610bb1a354268e638
Daniel Kinsbursky <dkinsbursky@kinsbursky.com>
2020-01-17 13:30:18 -0800

    Export Missing Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import {default as FormikForm} from './Formik/FormikForm';
 import {default as SubmitButton} from './Formik/SubmitButton';
 import {default as FormButton} from './Formik/FormButton';
-import {default as AutoComplete} from './Formik/AutoComplete';
-import {default as AutoCompleteObject} from './Formik/AutoCompleteObject';
+import {default as TextField} from './Formik/TextField';
 import {default as CurrencyField} from './Formik/CurrencyField';
 import {default as DateField} from './Formik/DateField';
 import {default as NumberField} from './Formik/NumberField';
 import {default as PasswordField} from './Formik/PasswordField';
-import {default as RadioGroup} from './Formik/RadioGroup';
 import {default as SelectField} from './Formik/SelectField';
 import {default as SwitchField} from './Formik/SwitchField';
-import {default as TextField} from './Formik/TextField';
+import {default as VinField} from './Formik/VinField';
 import {default as WeightField} from './Formik/WeightField';
+import {default as AutoComplete} from './Formik/AutoComplete';
+import {default as AutoCompleteObject} from './Formik/AutoCompleteObject';
+import {default as CheckboxGroup} from './Formik/CheckboxGroup';
+import {default as RadioGroup} from './Formik/RadioGroup';
 
 export {default as ValidationExample} from './TestComponent/ValidationExample';
 export * from './TestComponent/ValidationExample';
@@ -20,15 +22,17 @@ export const Formik = {
   FormikForm,
   SubmitButton,
   FormButton,
-  AutoComplete,
-  AutoCompleteObject,
+  TextField,
   CurrencyField,
   DateField,
   NumberField,
   PasswordField,
-  RadioGroup,
   SelectField,
   SwitchField,
-  TextField,
+  VinField,
   WeightField,
+  AutoComplete,
+  AutoCompleteObject,
+  CheckboxGroup,
+  RadioGroup,
 };
