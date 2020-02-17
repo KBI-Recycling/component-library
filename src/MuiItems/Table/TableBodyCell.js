e2e7e9f9779f0c77b5055b52b6a4f73e23890ca0
Daniel Kinsbursky <dkinsbursky@kinsbursky.com>
2020-02-17 14:48:38 -0800

    Update TableBodyCell.js

--- src/MuiItems/Table/TableBodyCell.js
+++ src/MuiItems/Table/TableBodyCell.js
@@ -4,45 +4,17 @@ import {ButtonGroup, Button, TableCell, Tooltip} from '@material-ui/core';
 import {Check, Close} from '@material-ui/icons';
 import moment from 'moment';
 
-
 const TableBodyCell = (props) => {
   const {cell} = props;
-  const actionsButtonStyle = useMemo(() => ({
-    border: '0px',
-    padding: '0px',
-  }), []);
+
   return (
     <TableCell {...cell.getCellProps()}>
       {cell.render((({cell}) => {
-        if (cell.column.actions) {
-          return (
-            <ButtonGroup size='small'>
-              {cell.column.actions.map((action, i) => {
-                if (!action?.tooltip) return <Button style={actionsButtonStyle} onClick={event => action.onClick({event})}>{action.icon}</Button>;
-                return (
-                  <Tooltip key={i} title={action.tooltip}>
-                    <Button style={actionsButtonStyle} onClick={event => action.onClick({event, rowData: cell.row.original, rowIndex: cell.row.index})}>
-                      {action.icon}
-                    </Button>
-                  </Tooltip>
-                );
-              })}
-            </ButtonGroup>
-          );
-        }
-        if (cell.column.type === 'boolean') {
-          return cell.value ? <Check /> : <Close />;
-        }
-        if (cell.column.type === 'currency') {
-          return cell.value.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
-        }
-        if (cell.column.type === 'datetime') {
-          const datetimeFormat = cell.column?.datetimeFormat ? cell.column.datetimeFormat : 'MM/DD/YYYY';
-          return moment(cell.value).format(datetimeFormat);
-        }
-        if (cell.column.type === 'numeric') {
-          return Number(cell.value).toLocaleString();
-        }
+        if (cell.column.actions) return <ActionsButtonGroup cell={cell} />;
+        if (cell.column.type === 'boolean') return cell.value ? <Check /> : <Close />;
+        if (cell.column.type === 'currency') return cell.value.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+        if (cell.column.type === 'datetime') return moment(cell.value).format(cell.column?.datetimeFormat ? cell.column.datetimeFormat : 'MM/DD/YYYY');
+        if (cell.column.type === 'numeric') return Number(cell.value).toLocaleString();
         return String(cell.value);
       }))}
     </TableCell>
@@ -53,3 +25,28 @@ TableBodyCell.propTypes = {
   cell: PropTypes.object.isRequired,
 };
 export default TableBodyCell;
+
+const ActionsButtonGroup = ({cell}) => {
+  const actionsButtonStyle = useMemo(() => ({
+    border: '0px',
+    padding: '0px',
+  }), []);
+
+  return (
+    <ButtonGroup size='small'>
+      {cell.column.actions.map((action, i) => {
+        if (!action?.tooltip) return <Button style={actionsButtonStyle} onClick={event => action.onClick({event})}>{action.icon}</Button>;
+        return (
+          <Tooltip key={i} title={action.tooltip}>
+            <Button style={actionsButtonStyle} onClick={event => action.onClick({event, rowData: cell.row.original, rowIndex: cell.row.index})}>
+              {action.icon}
+            </Button>
+          </Tooltip>
+        );
+      })}
+    </ButtonGroup>
+  );
+};
+ActionsButtonGroup.propTypes = {
+  cell: PropTypes.object.isRequired,
+};
