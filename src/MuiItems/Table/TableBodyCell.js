dc54cc0cd4e2e62101ca660aa7e85f1e986c1943
Daniel Kinsbursky <dkinsbursky@kinsbursky.com>
2020-02-17 15:19:50 -0800

    Update TableBodyCell.js

--- src/MuiItems/Table/TableBodyCell.js
+++ src/MuiItems/Table/TableBodyCell.js
@@ -1,21 +1,23 @@
 import React, {useMemo} from 'react';
 import PropTypes from 'prop-types';
-import {ButtonGroup, Button, TableCell, Tooltip} from '@material-ui/core';
-import {Check, Close} from '@material-ui/icons';
+import {ButtonGroup, Button, TableCell, Tooltip, Typography} from '@material-ui/core';
 import moment from 'moment';
 
 const TableBodyCell = (props) => {
   const {cell} = props;
+  const typoProps = useMemo(() => ({
+    variant: 'caption',
+  }), []);
 
   return (
     <TableCell {...cell.getCellProps()}>
       {cell.render((({cell}) => {
         if (cell.column.actions) return <ActionsButtonGroup cell={cell} />;
-        if (cell.column.type === 'boolean') return cell.value ? <Check /> : <Close />;
-        if (cell.column.type === 'currency') return cell.value.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
-        if (cell.column.type === 'datetime') return moment(cell.value).format(cell.column?.datetimeFormat ? cell.column.datetimeFormat : 'MM/DD/YYYY');
-        if (cell.column.type === 'numeric') return Number(cell.value).toLocaleString();
-        return String(cell.value);
+        if (cell.column.type === 'boolean') return <Typography {...typoProps}>{cell.value ? 'True' : 'False'}</Typography>;
+        if (cell.column.type === 'currency') return <Typography {...typoProps}>{cell.value.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</Typography>;
+        if (cell.column.type === 'datetime') return <Typography {...typoProps}>{moment(cell.value).format(cell.column?.datetimeFormat ? cell.column.datetimeFormat : 'MM/DD/YYYY')}</Typography>;
+        if (cell.column.type === 'numeric') return <Typography {...typoProps}>{Number(cell.value).toLocaleString()}</Typography>;
+        return <Typography {...typoProps}>{String(cell.value)}</Typography>;
       }))}
     </TableCell>
   );
@@ -35,11 +37,12 @@ const ActionsButtonGroup = ({cell}) => {
   return (
     <ButtonGroup size='small'>
       {cell.column.actions.map((action, i) => {
-        if (!action?.tooltip) return <Button style={actionsButtonStyle} onClick={event => action.onClick({event})}>{action.icon}</Button>;
+        const Icon = () => action.icon;
+        if (!action?.tooltip) return <Button style={actionsButtonStyle} onClick={event => action.onClick({event})}><Icon /></Button>;
         return (
           <Tooltip key={i} title={action.tooltip}>
             <Button style={actionsButtonStyle} onClick={event => action.onClick({event, rowData: cell.row.original, rowIndex: cell.row.index})}>
-              {action.icon}
+              <Icon style={{height: '100px'}} />
             </Button>
           </Tooltip>
         );
