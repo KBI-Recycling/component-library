2c8cb8b4262b662ce752ae17cd1b42e1d6f5d856
Daniel Kinsbursky <dkinsbursky@kinsbursky.com>
2020-02-21 15:07:21 -0800

    Update SpeedDialActions.js

--- src/MuiItems/Table/SpeedDialActions.js
+++ src/MuiItems/Table/SpeedDialActions.js
@@ -1,9 +1,11 @@
 import React, {useMemo, useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
+import {makeStyles} from '@material-ui/core/styles';
 
 import {SpeedDial, SpeedDialAction, SpeedDialIcon} from '@material-ui/lab';
 
 const SpeedDialActions = (props) => {
+  const styles = useStyles();
   const [open, setOpen] = useState(false);
   const actions = useMemo(() => {
     return [...props.actions];
@@ -11,17 +13,17 @@ const SpeedDialActions = (props) => {
 
   const speedDialProps = useMemo(() => ({
     ariaLabel: 'Table Actions',
-    hidden: false,
+    classes: {
+      root: styles.root,
+      fab: styles.fab,
+    },
+    direction: 'down',
+    hidden: actions.length === 0 ? true : false,
     icon: <SpeedDialIcon />,
     onClose: () => setOpen(false),
     onOpen: () => setOpen(true),
     open,
-    style: {
-      position: 'absolute',
-      bottom: '40px',
-      right: '40px',
-    },
-  }), [open]);
+  }), [actions.length, open, styles.fab, styles.root]);
   const speedDialActionProps = useCallback((action) => {
     return {
       onClick: () => alert(action.onClick),
@@ -40,6 +42,18 @@ const SpeedDialActions = (props) => {
   );
 };
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: 'absolute',
+    top: '16px',
+    right: '16px',
+  },
+  fab: {
+    width: '32px',
+    height: '32px',
+    opacity: '30%',
+  },
+}));
 SpeedDialActions.propTypes = {
   actions: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
