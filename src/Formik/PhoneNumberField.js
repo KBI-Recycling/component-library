d5bb61b1d43cf7a37a9983f59155e27c51cf60c7
gblackiv <blackmongerry@gmail.com>
2020-04-01 15:42:49 -0700

    always eslint warnings

--- src/Formik/PhoneNumberField.js
+++ src/Formik/PhoneNumberField.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import MuiTextField from '@material-ui/core/TextField';
 import {FastField, Field} from 'formik';
